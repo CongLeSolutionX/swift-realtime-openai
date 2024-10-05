3f56594a06baaf5cd58de0b466314ed7739fbefe
Cong Le <longchike@gmail.com>
2024-10-05 11:14:19 -0700

    update file structure and naming convention

--- Package.swift
+++ Package.swift
@@ -3,19 +3,13 @@
 import PackageDescription
 
 let package = Package(
-	name: "OpenAI",
-	platforms: [
-		.iOS(.v17),
-		.tvOS(.v17),
-		.macOS(.v14),
-		.watchOS(.v10),
-		.visionOS(.v1),
-		.macCatalyst(.v17),
-	],
-	products: [
-		.library(name: "OpenAI", targets: ["OpenAI"]),
-	],
-	targets: [
-		.target(name: "OpenAI", path: "./src"),
-	]
+    name: "OpenAI",
+    platforms: [.iOS(.v17), .tvOS(.v17), .macOS(.v14), .watchOS(.v10), .visionOS(.v1), .macCatalyst(.v17)],
+    products: [
+        .library(name: "OpenAI", targets: ["OpenAI"]),
+    ],
+    targets: [
+        .target(name: "OpenAI", dependencies: [], path: "Sources/OpenAI"),
+        .testTarget(name: "OpenAITests", dependencies: ["OpenAI"], path: "Tests/OpenAITests"),
+    ]
 )
